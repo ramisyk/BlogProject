378e2ff778516603295c8797106ed24ff21f214c
ramisyk <ramisyk@RAMISYK>
2021-09-11 16:26:47 +0300

    Entityframework packages are added and context is created in Data Access Layer

---
+++ DataAccess/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+            optionsBuilder.UseSqlServer(@"server=(localdb)\MSSQLLocalDB;database=BlogDb;Integrated Security=True;");
+        }
+    }
+}
