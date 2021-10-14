6637fa2d2234f73d4a515be44534a71d65e1ade2
ramisyk <ramisyk@RAMISYK>
2021-10-14 21:03:20 +0300

    create new comment view for blogs

---
+++ Presentation/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,20 @@
+﻿using Buesiness.Concrete;
+using DataAccess.EntityFtramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentation.ViewComponents.Comment
+{
+    public class CommentListByBlog:ViewComponent
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = commentManager.GetAll(8);
+            return View(values);
+        }
+    }
+}
