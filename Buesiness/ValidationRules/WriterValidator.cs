2d4ff4f2f8f4bd82d66b90010d6fa7481de08d3d
Ramis Yüksel <ramisyk97@gmail.com>
2021-10-20 21:38:24 +0300

    change valiadation layouts in register page

--- Buesiness/ValidationRules/WriterValidator.cs
+++ Buesiness/ValidationRules/WriterValidator.cs
@@ -15,6 +15,7 @@ namespace Buesiness.ValidationRules
             RuleFor(x => x.WriterName).NotEmpty().WithMessage("Name can't be null");
             RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail can't be null");
             RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Password can't be null");
+            RuleFor(x => x.WriterImage).NotEmpty().WithMessage("Image can't be null");
             RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Name has to include min 2 character");
             RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Name has to include max 50 character");
         }
